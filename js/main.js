4c1be99eb9c859af8f4464a06c72e689c3486bfe
ShahzaibIrfan01 <shahzaib.irfan1122@gmail.com>
2023-09-19 08:10:08 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// header scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 50){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let navcollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(e){
+    e.addEventListner("click" , function(){
+    navcollapse.classList.remove("show");
+})
+})
